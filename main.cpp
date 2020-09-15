e33b744d836788229c3a84c5039de8838598513c
Эльвира <st063765@student.spbu.ru>
2020-09-15 21:45:04 +0300

    Изменила класс Tranche на структуру с timestamp, hashValue, data (transactions), nounce. Изменила хэш-функцию от введенной структуры. Добавила класс Blockchain с одним методом createTranche. Закомметировала некоторые моменты.

--- main.cpp
+++ main.cpp
@@ -3,24 +3,36 @@
 
 using namespace std;
 
-class Tranche
-{
+class Blockchain{
 public:
+    void createTranche(){
+    };
+};
+
+//cтруктура блока в блокчейне, элементы которого:
+//timestamp -- время создания блока
+//transactions -- список транзакций
+//nounce -- некоторе целое число
+//hashValue -- значение хэш-функии от предыдущего блока
+struct Tranche{
     time_t timestamp;
     string transactions;
     int nounce;
-    string hash(){
-        string str;
-        //time(&timestamp);
-        str = ctime(&timestamp)+transactions + to_string(nounce);
-        return str;
-    }
+    double hashValue;
 };
+
+//хэш-функция(не дописанная): записывает в строку данные блока.
+string myhash(Tranche &block){
+    string str;
+    //time(&timestamp);
+    str = ctime(&block.timestamp)+block.transactions + to_string(block.nounce);
+    return str;
+}
 
 int main() {
     time_t current1;
     //cout<<hash1(current1, "X gave Y", nounce1)<<endl;
-    Tranche one {time(&current1),"X gave $100 to Y", 6};
-    cout<< one.hash()<<endl;
+    Tranche zero {time(&current1),"X gave $100 to Y", 6, 0};
+    cout<< myhash(zero)<<endl;
     return 0;
 }
