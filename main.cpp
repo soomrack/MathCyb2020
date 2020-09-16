525711ba3df2dc9dc24e1356b93e7938c5d35604
master_of_disaster <childhoodisend@users.noreply.github.com>
2020-09-16 16:49:06 +0300

    Update main.cpp
    Полностью переделана структура и класс

--- main.cpp
+++ main.cpp
@@ -1,43 +1,95 @@
 #include <iostream>
 #include <string>
+#include <cstdint>
 #include <ctime>
-#include <utility>
+#include <list>
+#include <fstream>
+#include<vector>
 
 using namespace std;
 
-struct Block{
-    string data, name, hash; // data, name, hash
-    int nounce; // some nounce
-    time_t time_stamp; //time
-
-    Block(string name, string data, string hash, int nounce) : name(std::move(name)),data(std::move(data)),hash(std::move(hash)), nounce(nounce) { // construct block
-        time_stamp = time(nullptr);
-    }
+struct Block {
+private:
+    string data, name;
+    uint64_t nounce{}, hash{};
+    time_t time_stamp{};
+public:
+    Block();
+    Block(const string& name, const string& data, uint64_t hash, uint64_t nounce);
+    //Block(Block& block);
+    //~Block();
 };
 
-string make_hash(){ // hash_function
-    return "hash";
+Block::Block() = default;
+Block::Block(const string & name, const string & data, uint64_t hash, uint64_t nounce) {}
+
+class BlockChain {
+private:
+    static list<Block> chain;
+public:
+
+    /*
+     *Конструкторы
+    */
+    BlockChain();
+    explicit BlockChain(const Block& block);
+    explicit BlockChain(const list<Block>& block);
+    /*
+     * Интерфейс
+    */
+    static list<Block> push(const Block& newTail); //Добавление блока в конец
+    static list<Block> pop(); //Удаление последнего блока из цепи, возвращает остаток цепочки
+    static void print_last_messages(int n); //Печать последних n сообщений
+    static int chain_length(); // Длина цепи
+    /*
+     * Работа с файлами
+     */
+    static void Save_to_file(FILE* file); // Сохранение в файл цепи
+    static void Load_from_file(FILE* file); // Загрузка цепи из файла
+};
+
+BlockChain::BlockChain() = default;
+BlockChain::BlockChain(const Block & block) {/*chain.push_back(block)*/}
+BlockChain::BlockChain(const list<Block>& block) {}
+
+/*int BlockChain::chain_length() {return chain.size();}*/
+
+/*list<Block> BlockChain::push(const Block& newTail) {
+//    chain.push_back(newTail);
+//    return chain;
 }
 
-class Chain{ // block_chain class
+list<Block> BlockChain::pop() {
+//    chain.pop_back();
+//    return chain;
+}*/
 
-    static Block *block;
-    Chain *next;
+void BlockChain::print_last_messages(int n) {}
 
+void BlockChain::Save_to_file(FILE *file) {
+//    if(!file) {
+//       cout << "Invalid name, saving -> BlockChain_saver.txt" << endl;
+//       ofstream output("BlockChain_saver.txt");
+//    }
+//    else
+//        ofstream output(file);
+//
+//    for (auto i = chain.begin(); i != chain.end() ; ++i) {
+//
+//    }
+}
 
-    static void entering_data(){
-        int nounce;
-        cin >> nounce;
-        string name, data, hash;
-        cin >> name >> data;
-        hash = make_hash();
-        block->data = data;
-        block->name = name;
-        block->hash = hash;
-        block->nounce = nounce;
-
+void BlockChain::Load_from_file(FILE *file) {
+    if(file){
+        //DOSMTH
     }
-};
+    else
+        cerr << "There is no file!";
+}
+
+string make_hash(){ // hash_function
+    return "none\n";
+}
 
 int main() {
 
