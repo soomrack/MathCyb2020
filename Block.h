2c046feca70548be8539c9b24de85c7bb38f73ae
Krylov Sergey <kssergeyru@gmail.com>
2020-09-16 12:01:26 +0300

    Initial commit - dirty implementation

---
+++ Block.h
@@ -0,0 +1,36 @@
+//
+// Created by Sergey on 16.09.2020.
+//
+
+#ifndef MATHCYB2020_BLOCK_H
+#define MATHCYB2020_BLOCK_H
+
+#include <cstdint>
+#include <iostream>
+#include <sstream>
+
+using namespace std;
+
+class Block {
+public:
+    string get;
+
+    Block(const string data);
+
+    string GetHash();
+
+    static void MineBlock(Block lastBlock, string data);
+
+private:
+    int difficulty;
+    int nonce;
+    string data;
+    string hash;
+    string lastHash;
+    time_t timestamp;
+
+    string _CalculateHash() const;
+};
+
+
+#endif //MATHCYB2020_BLOCK_H
---
+++ Blockchain.h
@@ -0,0 +1,27 @@
+//
+// Created by Sergey on 16.09.2020.
+//
+
+#ifndef MATHCYB2020_BLOCKCHAIN_H
+#define MATHCYB2020_BLOCKCHAIN_H
+
+
+#include "Block.h"
+
+#include <cstdint>
+#include <iostream>
+#include <sstream>
+
+using namespace std;
+
+class Blockchain {
+public:
+    Blockchain();
+
+    void addBlock(Block block);
+
+    vector<Block> chain;
+};
+
+
+#endif //MATHCYB2020_BLOCKCHAIN_H
