8b3629091c45a21837e11be4ad1671ab790ff129
Эльвира <st063765@student.spbu.ru>
2020-09-21 16:56:27 +0300

    Подправила код по образцу с занятия.

--- main.cpp
+++ main.cpp
@@ -1,13 +1,7 @@
 #include <iostream>
 #include <ctime>
-
+#include <list>
 using namespace std;
-
-class Blockchain{
-public:
-    void createTranche(){
-    };
-};
 
 //cтруктура блока в блокчейне, элементы которого:
 //timestamp -- время создания блока
@@ -17,10 +11,40 @@ public:
 struct Tranche{
     time_t timestamp;
     string transactions;
-    int nounce;
-    double hashValue;
+    uint64_t nounce;
+    uint64_t hashValue;
+public:
+    Tranche();
+    Tranche(const string transactions, const uint64_t nounce, const uint64_t hashValue);
+    Tranche(const Tranche &tranche);
+
+private:
+    ~Tranche();
 };
 
+class Blockchain {
+private:
+    std::list<Tranche> chain;
+
+public:
+    Blockchain();
+
+private:
+    ~Blockchain();
+
+public:
+    int push(const Tranche new_tail); //добавляет новый блок к концу блокчейна
+    Tranche pop(); // Удаляет хвостовой блок из цепочки и возвращает его
+
+public:
+    int save_to_file(const string filename);
+
+    int load_from_file(const string filename);
+
+public:
+    int print_last_message(const int n); // Выводит на консоль сообщения из крайних n блоков
+    // и количество выведенных сообщений
+};
 //хэш-функция(не дописанная): записывает в строку данные блока.
 string myhash(Tranche &block){
     string str;
@@ -30,9 +54,9 @@ string myhash(Tranche &block){
 }
 
 int main() {
-    time_t current1;
-    //cout<<hash1(current1, "X gave Y", nounce1)<<endl;
-    Tranche zero {time(&current1),"X gave $100 to Y", 6, 0};
-    cout<< myhash(zero)<<endl;
+//    time_t current1;
+//    //cout<<hash1(current1, "X gave Y", nounce1)<<endl;
+//    Tranche zero {time(&current1),"X gave $100 to Y", 6, 0};
+//    cout<< myhash(zero)<<endl;
     return 0;
 }
