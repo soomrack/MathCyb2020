6928460ecab201ec57755250df91aa68b278d608
master_of_disaster <childhoodisend@users.noreply.github.com>
2020-09-21 15:49:56 +0300

    new commit

---
+++ cmake-build-debug/CMakeFiles/MathCyb2020.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/MathCyb2020.dir/main.cpp.obj"
+  "MathCyb2020.exe"
+  "MathCyb2020.exe.manifest"
+  "MathCyb2020.lib"
+  "MathCyb2020.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/MathCyb2020.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
