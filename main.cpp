631d4bdc956949c60a5a5e43ead9568f5b958ecd
Эльвира <st063765@student.spbu.ru>
2020-10-13 21:49:15 +0300

    Сделала Tranche вместо структуры классом (мне так захотелось). Описала конструкторы для Tranche, и убедилась, что они работают. Добавила методы, которые выдают компоненты объекта, т.к. они у нас private. Эти методы используются в функции myhash. Также попыталась реализовать сохранение в файл.

--- main.cpp
+++ main.cpp
@@ -2,6 +2,7 @@
 #include <ctime>
 #include <list>
 #include <string.h>
+#include <fstream>
 
 using namespace std;
 
@@ -10,7 +11,7 @@ using namespace std;
 //transactions -- список транзакций
 //nounce -- некоторе целое число
 //hashValue -- значение хэш-функии от предыдущего блока
-struct Tranche{
+class Tranche{
     time_t timestamp;
     string transactions;
     uint64_t nounce;
@@ -18,12 +19,61 @@ struct Tranche{
 
 public:
     Tranche();
-    Tranche(const string transactions, const uint64_t nounce, const uint64_t hashValue);
+    Tranche(const time_t timestamp, const string transactions, const uint64_t nounce, const uint64_t hashValue);
     Tranche(const Tranche &tranche);
 
 public:
     ~Tranche();// деструктор
+
+public:
+    time_t get_timestamp();
+    string get_transactions();
+    uint64_t get_nounce();
+    uint64_t get_hashValue();
 };
+
+
+Tranche::Tranche() {
+    time_t now;
+    timestamp = time(&now);
+    transactions = "default transactions";
+    nounce = 0;
+    hashValue = 8;
+}
+
+Tranche::Tranche(const time_t new_timestamp, const string new_transactions, const uint64_t new_nounce, const uint64_t new_hashValue) {
+    timestamp = new_timestamp;
+    transactions = new_transactions;
+    nounce = new_nounce;
+    hashValue = new_hashValue;
+}
+
+Tranche::Tranche(const Tranche &tranche) {
+    timestamp = tranche.timestamp;
+    transactions = tranche.transactions;
+    nounce = tranche.nounce;
+    hashValue = tranche.hashValue;
+}
+
+time_t Tranche::get_timestamp() {
+    return timestamp;
+}
+
+string Tranche::get_transactions() {
+    return transactions;
+}
+
+uint64_t Tranche::get_nounce() {
+    return nounce;
+}
+
+uint64_t Tranche::get_hashValue() {
+    return hashValue;
+}
+
+Tranche::~Tranche() {
+
+}
 
 class Blockchain {
 private:
@@ -59,32 +109,44 @@ Tranche Blockchain::pop() {
     Tranche tail_block(*index); //здесь CLion попросил сделать деструктор Tranche public вместо private
     chain.pop_back();
     return tail_block;
-}
+};
 
 int Blockchain::save_to_file(const string filename) {
+    ofstream outFile;
+    outFile.open(filename);
 
-}
+    // здесь надо как-то записать блокчейн в файл
+    for (auto element : chain) outFile << element << endl; // не работает так! надо разобраться
+    outFile.close();
+    return 0;
+};
 
 int Blockchain::load_from_file(const string filename) {
+    ifstream inFile;
+    inFile.open(filename);
 
-}
+    // тут надо как-то загрузить блокчейн
+
+    inFile.close();
+    return 0;
+};
 
 int Blockchain::print_last_message(const int n) {
-
-}
+    return 0;
+};
 
 //хэш-функция(не дописанная): записывает в строку данные блока.
 string myhash(Tranche &block){
     string str;
     //time(&timestamp);
-    str = ctime(&block.timestamp)+block.transactions + to_string(block.nounce);
+    str = to_string(block.get_timestamp())+block.get_transactions() + to_string(block.get_nounce());
     return str;
-}
+};
 
 int main() {
-//    time_t current1;
-//    //cout<<hash1(current1, "X gave Y", nounce1)<<endl;
-//    Tranche zero {time(&current1),"X gave $100 to Y", 6, 0};
-//    cout<< myhash(zero)<<endl;
+    time_t current1;
+    //cout<<hash1(current1, "X gave Y", nounce1)<<endl;
+    Tranche zero(time(&current1),"X gave $100 to Y", 6, 0);
+    //cout<< myhash(zero)<<endl;
     return 0;
 }
