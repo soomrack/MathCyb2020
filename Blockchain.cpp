b0b61643a4b72dd9917f037369919bff42dae3be
easyri <45106182+easyri@users.noreply.github.com>
2020-10-14 13:47:03 +0300

    частично прописаны функции для синхронизации двух блокчейнов

--- Blockchain.cpp
+++ Blockchain.cpp
@@ -1,6 +1,3 @@
-//
-// Created by belyn on 16.09.2020.
-//
 
 #include "Blockchain.h"
 #include <stdexcept>
@@ -78,10 +75,7 @@ int Blockchain::save_to_file(string filename) {
     output_file.open(filename, std::ios_base::trunc);
     if(output_file.fail())
         std::cout<<"unable to open the file"<<endl;
-//    ostream_iterator<Blockchain> output_iterator(output_file, "\n");
-//    copy(chain.begin(), chain.end(), output_iterator);
     for(const auto &e : chain) output_file << e << "\n";
-//    output_file<<"hello";
     output_file.close();
     return 0;
 }
@@ -92,4 +86,74 @@ int Blockchain::load_from_file(string filename) {
     vector<Block> chain_(start, end);
     chain = chain_;
     return 0;
+}
+
+
+// синхронизация
+int Blockchain::send_length(string address){
+    ofstream output_file;
+    output_file.open(address, std::ios_base::trunc);
+    if(output_file.fail())
+    {
+        std::cout<<"unable to send"<<endl;
+        return 1;
+    }
+    output_file << len() << "\n";
+    output_file.close();
+    return 0;
+}
+
+bool Blockchain::check_length(string address){
+    ifstream is(address);
+    int read_len;
+    is >> read_len;
+    if (len() >= read_len) return true;
+    return false;
+}
+
+bool Blockchain::check_same(Block block){
+    if(hash(block) == hash(chain[chain.size() - 1])) return true;
+    return false;
+}
+
+int Blockchain::send_block(string address, int i){
+    Block block = chain[i];
+    ofstream output_file;
+    output_file.open(address, std::ios_base::trunc);
+    if(output_file.fail())
+    {
+        std::cout<<"unable to send"<<endl;
+        return 1;
+    }
+    output_file << block << "\n";
+    output_file.close();
+    return 0;
+}
+
+int Blockchain::send_success(string address){
+    ofstream output_file;
+    output_file.open(address, std::ios_base::trunc);
+    if(output_file.fail())
+    {
+        std::cout<<"unable to send"<<endl;
+        return 1;
+    }
+    output_file << 0 << "\n";
+    output_file.close();
+    return 0;
+}
+
+// TODO
+int Blockchain::sync(string alice_len_address, string my_len_address,
+        string alice_block_address, string my_block_address,
+        string alice_success_address, string my_success_address){
+    send_length(my_len_address);
+    if(check_length(alice_len_address))   // моя цепь длиннее - читаем блоки алисы, пока не совпадают,
+        // записываем в temp_chain, присоединяем, посылаем алисе success
+    {
+        while(true){
+
+        }
+    }
+    // иначе посылаем алисе блоки, она она не пошлет флаг на my_success_address
 }
--- Blockchain.h
+++ Blockchain.h
@@ -12,13 +12,10 @@
 using namespace std;
 
 class Blockchain {
-private:
-    vector<Block> chain;
-    uint64_t M = 10; // мощность сети
-
 public:
     Blockchain();
-
+    vector<Block> chain;
+    uint64_t M = 10; // мощность сети
     Blockchain(Block block);
     Blockchain(Block block, uint64_t M);
     Blockchain(vector<Block> &v);
@@ -31,6 +28,18 @@ public:
     int load_from_file(string filename);
     int print_last_messages(int n);
     friend ostream& operator<< (ostream &out, const Blockchain &chain);
+
+    // синхронизация
+    vector<Block> temp_chain;
+    int n_same = len();     // номер крайнего совпадающего блока
+    int send_length(string address);
+    bool check_length(string address);
+    bool check_same(Block block);       // проверка совпадения последних блоков
+    int send_block(string address, int i);
+    int send_success(string address);
+    int sync(string alice_len_address, string my_len_address,
+            string alice_block_address, string my_block_address,
+             string alice_success_address, string my_success_address);
 
 
 };
