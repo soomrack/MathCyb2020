eecfda58746b955fb2c4eb957e54ea05bf609316
Galina Reneva <renevag@gmail.com>
2020-10-14 13:34:11 +0300

    Partially implemented interface for synchronization.
    Also compare blocks and get length functions are added.

--- main.cpp
+++ main.cpp
@@ -12,11 +12,13 @@ private:
     string data;
     uint64_t nounce;
     uint64_t prev_hash;
+    uint64_t index;
 
 public:
     Block(const string &data, const uint64_t nounce, const uint64_t prev_hash)
             : timestamp(), data(data), nounce(nounce), prev_hash(prev_hash) {
         time(&this->timestamp);
+        index = 0;
     }
 
     uint64_t computeHash() const {
@@ -31,9 +33,23 @@ public:
         return this->data;
     }
 
+    uint64_t getIndex() const {
+        return this->index;
+    }
+
+    void setIndex(uint64_t newIndex) {
+        this->index = newIndex;
+    }
+
 public:
     ~Block() = default;
 };
+
+bool compareBlocks(const Block &block1, const Block &block2) {
+    if (block1.computeHash() == block2.computeHash())
+        return true;
+    return false;
+}
 
 class BlockChain {
 private:
@@ -42,39 +58,70 @@ private:
 public:
     BlockChain() : blockchain() {}
 
-    bool push(const Block &block) {
-        if (!blockchain.empty() && (blockchain.back().computeHash() != block.getPrevHash()))
-            return false;
+    bool push(const Block &block);
+    bool pop();
 
-        this->blockchain.push_back(block);
-        return true;
-    }
-
-    bool pop() {
-        if (blockchain.empty())
-            return false;
-        this->blockchain.pop_back();
-        return true;
-    }
-
-    bool printLastData(int n){
-        if (n > blockchain.size())
-            return false;
-        auto iter = blockchain.rbegin();
-        for (int i = 0; i < n; ++i) {
-            cout << iter->getData();
-            iter++;
-        }
-        return true;
-    }
+    bool printLastData(int n);
 
     bool saveToFile(const string &filename);
-
     bool loadFromFile(const string &filename);
+
+    /* Так как пока нет взаимодействия по сети, то отправка данных заключается
+    * в вызове у другого блокчейна функции, обрабатывающей отправленную информацию*/
+    bool sendLength(BlockChain &receiver){
+        receiver.onLengthReceived(*this, blockchain.size());
+    }
+
+    bool sendBlock(BlockChain &receiver, const Block &block){
+        receiver.onBlockReceived(*this, block);
+    }
+
+    bool sendSuccessMessage(BlockChain &receiver){
+        receiver.onSuccessMessageReceived(*this, "Success");
+    }
+
+    bool onLengthReceived(BlockChain &sender, const uint64_t anotherLength);
+    bool onBlockReceived(BlockChain &sender, const Block &anotherBlock);
+    bool onSuccessMessageReceived(BlockChain &sender, const string &successMessage);
+
+    bool synchronization(BlockChain &anotherBlockChain);
+
+    uint64_t getLength(){
+        return blockchain.size();
+    }
 
 public:
     ~BlockChain() = default;
 };
+
+bool BlockChain::push(const Block &block) {
+    if (!blockchain.empty() && (blockchain.back().computeHash() != block.getPrevHash()))
+        return false;
+
+    this->blockchain.push_back(block);
+    blockchain.back().setIndex(blockchain.size() - 1);
+    return true;
+}
+
+bool BlockChain::pop() {
+    if (blockchain.empty())
+        return false;
+    this->blockchain.pop_back();
+    return true;
+}
+
+bool synchronization(const BlockChain &anotherBlockChain){
+
+}
+
+bool BlockChain::printLastData(int n){
+    auto iter = blockchain.rbegin();
+    for (int i = 0; i < n && i < blockchain.size(); ++i) {
+        cout << iter->getData();
+        iter++;
+    }
+    return true;
+}
 
 int main() {
     return 0;
