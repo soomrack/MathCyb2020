47181c84cac446dc16b1813c8da15dfc06d79ef5
evgenia <zhenyatitoova@gmail.com>
2020-10-20 21:46:57 +0300

    Убрала openssl, добавила новую функцию подсчета sha256

--- lib/src/block.cpp
+++ lib/src/block.cpp
@@ -1,6 +1,6 @@
 #include "block.h"
 
-#include "openssl/sha.h"
+#include "sha256.h"
 
 Block::Block() {}
 
@@ -10,7 +10,8 @@ Block::Block ( const uint64_t nounce, const std::string hash, const std::string 
     timestamp = boost::posix_time::second_clock::universal_time();
 }
 
-Block::Block ( const uint64_t nounce, const boost::posix_time::ptime timestamp, const std::string hash, const std::string message ) :
+Block::Block ( const uint64_t nounce, const boost::posix_time::ptime timestamp,
+               const std::string hash, const std::string message ) :
         nounce( nounce ), timestamp( timestamp ), hash( hash ), message( message )
 {
 
@@ -29,17 +30,8 @@ Block::~Block()
 
 std::string Block::getHash()
 {
-    std::string data = std::to_string( nounce ) +
-            boost::posix_time::to_iso_string( timestamp ) + hash + message;
-    unsigned char hash[SHA256_DIGEST_LENGTH];
-    SHA256_CTX sha256;
-    SHA256_Init(&sha256);
-    SHA256_Update(&sha256, data.c_str(), data.size());
-    SHA256_Final(hash, &sha256);
-    std::stringstream ss;
-    for( int i = 0; i < SHA256_DIGEST_LENGTH; i++ )
-    {
-        ss << std::hex << std::setw(2) << std::setfill('0') << ( int )hash[i];
-    }
-    return ss.str();
+    std::string input = boost::lexical_cast<std::string>( nounce ) +
+                        boost::posix_time::to_iso_string( timestamp ) +
+                        hash + message;
+    return sha256( input );
 }
--- readme.txt
+++ readme.txt
@@ -1,18 +1,12 @@
 Блокчейн.
 Практические занятия.
 
-Требуется использование библиотек OpenSSL и Boost.
+Требуется использование Boost.
 
-Установка Boost (mingw):
+Установка Boost:
 1. Скачать файл boost_1_73_0.zip (Windows) или boost_1_73_0.tar.bz2 (linux)
    с официального сайта boost https://www.boost.org/users/history/version_1_73_0.html
 2. Распаковать архив в C:\Program Files\boost\boost_1_73_0 для Windows или
    /usr/local/boost_1_73_0 для linux
 3. Библиотека скомпилируется и будет найдена средствами cmake
-
-Установка Openssl (mingw):
-1. Так как вся команда использует Anaconda3 в учебных целях, эта библиотека уже
-   скачана и может быть просто найдена cmake
-2. В противном случае эта библиотека поставляется вместе с git, так что нужно
-   запустить C:\Program Files\Git\usr\bin\openssl.exe
 
