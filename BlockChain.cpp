16cafad139dc20b37a9ca0922e1817fdeaf28aa5
LargeMangoFromTheCave <61617124+LargeMangoFromTheCave@users.noreply.github.com>
2020-10-28 12:41:57 +0300

    Update BlockChain.cpp

--- BlockChain.cpp
+++ BlockChain.cpp
@@ -6,12 +6,12 @@
 
 BlockChain::BlockChain() {
     Chain.emplace_back(Block(0, "Genesis Block"));
-//    Difficulty = 6;
+    Difficulty = 6;
 }
 
 void BlockChain::AddBlock(Block B_New) {
     B_New.prev_hash = GetLastBlock().GetHash();
-//    B_New.MiningBlock(Difficulty);
+    B_New.MiningBlock(Difficulty);
     Chain.push_back(B_New);
 }
 
@@ -23,8 +23,15 @@ int BlockChain::get_length() {
     return Chain.size();
 }
 
+// Synchronization
+
 int send_length(string address, int length);
  
 int send_block(string address, Block block, int block_number);
 
 int send_success(string address);
+
+int load_data(string address);
+
+// Нужна функция для вывода последнего совпадающего блока в двух блокчейнах. 
+// Для этого нужно получить эти два блокчейна. Также нужна проверять хэш или 
